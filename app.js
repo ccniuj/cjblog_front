33dcb1912d3301c826c36f85f3c93528f2253345
Juin Chiu <davidjuin0519@gmail.com>
2016-05-26 14:07:21 +0800

    add passenger file

--- app.js
+++ app.js
@@ -3,14 +3,13 @@ var path = require('path')
 var compression = require('compression')
 var app = express()
 var server = require('http').Server(app)
-var env = process.env.ENV ? process.env.ENV : 'production';
-var config = require('./config.json')[env]
 
 app.use(compression())
 app.use(express.static(path.join(__dirname, 'public')))
 app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
-server.listen(config.port, function() {
-  console.log('Server start listening on port ' + config.port)
+var PORT = process.env.PORT || 8080;
+server.listen(PORT, function() {
+  console.log('Server start listening on port ' + PORT)
 })
