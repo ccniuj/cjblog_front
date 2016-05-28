d01d07bf5a0f0beeafad6092d3ac3b463cdf777f
Juin Chiu <davidjuin0519@gmail.com>
2016-05-29 00:51:12 +0800

    add simple editor

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 import AboutBox   from './components/about/AboutBox'
 import ArticleBox from './components/articles/ArticleBox'
-import EditBox    from './components/commons/Navbar'
+import EditorBox  from './components/dashboard/EditorBox'
 import Navbar     from './components/commons/Navbar'
 
-import 'bootstrap-webpack'
+import 'bootstrap-webpack!./bootstrap.config.js'
 import 'font-awesome-webpack'
 import './assets/stylesheets/style.css'
+import './assets/stylesheets/draft.css'
+import './assets/stylesheets/editor.css'
 
 ReactDOM.render((
   <Router history={browserHistory}>
     <Route path='/' component={Navbar}>
       <IndexRoute component={AboutBox} />
       <Route path='/articles' component={ArticleBox} />
-      <Route path='/edit' component={EditBox} />
+      <Route path='/editor' component={EditorBox} />
     </Route>
   </Router>
 ), document.getElementById('app'))
