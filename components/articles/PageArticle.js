e7b1b4f5364e5049aace0f347bec1d3d37f6232e
Juin Chiu <davidjuin0519@gmail.com>
2016-06-22 22:18:13 +0800

    add identifier to disqus

--- components/articles/PageArticle.js
+++ components/articles/PageArticle.js
@@ -69,6 +69,7 @@ export default class extends React.Component {
           <div className='article-content' dangerouslySetInnerHTML={{__html: this.state.data.text}} />
           <Disqus
             shortname="cjcjblog"
+            identifier={this.state.data.id}
             url={window.location.href}
             onNewComment={() => console.log('new comment')}/>
         </div>
