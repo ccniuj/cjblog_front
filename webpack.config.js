7d506ba89cfc69ed1ec49f39a701a3f40f35b95f
Juin Chiu <davidjuin0519@gmail.com>
2016-08-04 23:17:58 +0800

    config webpack, remove some unneeded fiels

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,39 @@
-var webpack = require('webpack');
-var env = process.env.ENV ? process.env.ENV : 'production';
+var path = require('path')
+var webpack = require('webpack')
+var env = process.env.ENV ? process.env.ENV : 'production'
 
 module.exports = {
+  devtool: 'cheap-module-eval-source-map',
+  entry: [
+    'webpack-hot-middleware/client',
+    './client/index.js'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/',
+  },
   plugins: [
-    new webpack.ProvidePlugin({
-      React: 'react',
-      ReactDOM: 'react-dom',
-      $: 'jquery',
-      jQuery: 'jquery',
-      'window.jQuery': 'jquery'
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': JSON.stringify({ENV: env})
     })
   ],
-  entry: {
-    'js/index': './index.js'
-  },
-  output: {
-    path: 'public/bundles',
-    publicPath: '/bundles/',
-    filename: '[name].js'
-  },
-  externals: {
-    'Config': JSON.stringify(require('./config.json')[env])
-  },
   module: {
     loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' },
       {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel',
-        query: {
-          presets: ['es2015']
-        }
+        test: /\.js$/,
+        loaders: [ 'babel' ],
+        exclude: /node_modules/,
+        include: __dirname,
       },
-      { test: /\.css$/, loader: "style!css" },
-      { test: /\.scss$/, loaders: ["style", "css", "sass"] },
-      { test: /\.json$/, loader: 'json'},
-      { test: /\.(png|jpg|gif)$/, loader: 'url-loader?limit=8192' },
-      { test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/, query: { presets:['react'] } },
-      { test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
-      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
-      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
-      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' }
+      {
+        test: /\.json$/,
+        loaders: [ 'json' ],
+        exclude: /node_modules/,
+        include: __dirname,
+      }
     ]
-  },
-  devtool: '#inline-source-map'
-};
+  }
+}
